d7faf51de8d9903d9e3ecbec484ddf48478ee281
Jonathan Morales <jony.hackmc@gmail.com>
2021-03-15 14:08:39 -0600

    Continuidad de preparacion...

--- lib/src/models/persona_usuario_model.dart
+++ lib/src/models/persona_usuario_model.dart
@@ -1,11 +1,17 @@
 //import 'package:flutter/material.dart';
 import 'package:app4/src/models/abstracts/abstrac_model.dart';
+import 'package:app4/src/models/persona_model.dart';
+import 'package:app4/src/models/usuario_model.dart';
 //import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 class PersonaUsuario implements ModelAbs {
   @override
   int id;
   int get getID => id;
+  Persona persona;
+  Usuario usuario;
+
+  PersonaUsuario({this.id, this.persona, this.usuario});
 
   @override
   deJsonAObj(Map<String, dynamic> json) {}
@@ -13,5 +19,51 @@ class PersonaUsuario implements ModelAbs {
   @override
   Map<String, dynamic> getDataJson() {
     return {};
+  }
+}
+
+class ColorDataSource extends RecursosDatos {
+  @override
+  Object getValue(ModelAbs model, String nombreColumna) {
+    PersonaUsuario perUser = model; // bajar hasta el hijo
+    switch (nombreColumna) {
+      case 'id':
+        return perUser.id;
+        break;
+      case 'nombre':
+        return perUser.persona.nombre;
+        break;
+      case 'apellido_paterno':
+        return perUser.persona.apellidoPaterno;
+        break;
+      case 'apellido_materno':
+        return perUser.persona.apellidoMaterno;
+        break;
+      case 'sexo':
+        return perUser.persona.sexo;
+        break;
+      case 'usuario':
+        return perUser.usuario.usuario;
+        break;
+      case 'pass':
+        return perUser.usuario.contrasena;
+        break;
+      case 'contra':
+        return perUser.usuario.permisos;
+        break;
+    }
+  }
+
+  @override
+  List<PersonaUsuario> get dataSource => lista;
+
+  @override
+  setlista(List<ModelAbs> lista) {
+    this.lista = lista;
+  }
+
+  @override
+  void updateDataGridSource() {
+    notifyListeners();
   }
 }
--- lib/src/providers/usuario_persona_providers.dart
+++ lib/src/providers/usuario_persona_providers.dart
@@ -1,4 +1,7 @@
 import 'package:app4/src/models/abstracts/abstrac_model.dart';
+import 'package:app4/src/models/persona_model.dart';
+import 'package:app4/src/models/persona_usuario_model.dart';
+import 'package:app4/src/models/usuario_model.dart';
 import 'package:app4/src/providers/persona_providers.dart';
 import 'package:app4/src/providers/usuario_providers.dart';
 
@@ -23,7 +26,22 @@ class PersonaUsuarioProvider extends ProviderAbs {
 
   @override
   Future<List<ModelAbs>> getData({ModelAbs model}) async {
-    return [];
+    List<Persona> personList = await persona.getData(
+        model:
+            Persona(id: model.id)); // primero se traeran los datos de persona
+    List<Usuario> userList =
+        await persona.getData(model: Usuario(id: model.id));
+
+    List<PersonaUsuario> personsUsers = [];
+    for (var i = 0; i < personList.length; i++) {
+      if (personList[i].id == userList[i].id) {
+        personsUsers.add(new PersonaUsuario(
+            id: personList[i].id,
+            persona: personList[i],
+            usuario: userList[i]));
+      }
+    }
+    return personsUsers;
   }
 
   @override
